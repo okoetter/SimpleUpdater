da9ba45876575b491853673c819c7bac8781499f
Oliver Kötter <oliver.koetter@tns-infratest.com>
2016-01-18 11:55:01 +0100

    updated version and copyright message of Updater.exe

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Updater")]
 [assembly: AssemblyDescription("A simple updater that can update an application from the internet.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jarrett Vance")]
+[assembly: AssemblyCompany("Jarrett Vance, Oliver Kötter")]
 [assembly: AssemblyProduct("Updater")]
-[assembly: AssemblyCopyright("Copyright © Jarrett Vance 2012")]
+[assembly: AssemblyCopyright("Copyright © Jarrett Vance 2012, Oliver Kötter 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
